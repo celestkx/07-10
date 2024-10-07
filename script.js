e55c4c587a2c68ce25942d5f3bccec815c7e4fed
KAIQUE PEREIRA DA CONCEICAO <kaique.pconceicao1@senacsp.edu.br>
2024-10-07 17:25:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// let fruta = ["abacate","melão","kiwi", "maça"];
+
+// console.log(fruta[0]);
+// fruta.push("manga");
+// console.log(fruta[fruta.length -1]);
+// console.log(fruta);
+
+// for (let i = 0; i < fruta.length; i ++){
+//     console.log(fruta[i]);
+// }
+
+
+
+// let marcas = ["samsung","motorola","apple","xiaomi"]
+
+// console.log(marcas);
+// console.log(marcas[marcas.length-1]);
+
+// for (let m = 0; m < marcas.length; m++){
+//     console.log(marcas[m]);
+// }
+
+// let marcas = ["samsung","motorola","apple","xiaomi"]
+
+// let novasMarcas = marcas.map((marcas) => {
+//     return marcas.length;
+// });
+// console.log(novasMarcas); 
+
+// let legumes = ["batata", "cenoura", "beterraba", "beringela", "chuchu"]
+
+// legumesComB = legumes.find(b => b.startsWith ("b"));
+
+// function saudacao (nome) {
+//     console.log(`Ola ${nome}`);
+// }
+// saudacao ("Joaquim");
+// saudacao ("Cristiano");
+// saudacao ("Jorge");
+// saudacao ("Bianca");
+// saudacao ("Cleuza");
+
+// function despedida (nome){
+//     console.log(`Até logo ${nome}`);
+// }
+// despedida ("Joaquim");
+
+
+// function soma (a,b){
+//     return a + b;
+// }
+// console.log (soma(1,2));
+
+// function multiplicacao (a,b){
+//     return a*b
+// } console.log (multiplicacao(1,2));
+
+// let somaAnonima = function (a,b){
+//     return a+b
+// }
+// console.log (somaAnonima(1,2));
+
+function saudacao (nome) {
+    return `Olá, ${nome}!`;
+}
+console.log (saudacao ("João")); 
+
+
+function soma (a,b){
+    return a+b
+}
+console.log (soma(1,2));
+
+
+
+
+
+// o primeiro comprimento de um array estará na posição zero
